24c4484a31e60d5e3ce252c7a0b661b2e4cad4e1
matt-edmondson <matthew.edmondson@gmail.com>
2024-06-11 15:04:38 +1000

    Add documentation to SignificantNumberExtensions

--- SignificantNumber/SignificantNumberExtensions.cs
+++ SignificantNumber/SignificantNumberExtensions.cs
@@ -3,8 +3,17 @@ namespace ktsu.io.SignificantNumber;
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 
+/// <summary>
+/// Provides extension methods for converting numbers to <see cref="SignificantNumber"/>.
+/// </summary>
 public static class SignificantNumberExtensions
 {
+	/// <summary>
+	/// Converts the input number to a <see cref="SignificantNumber"/>.
+	/// </summary>
+	/// <typeparam name="TInput">The type of the input number.</typeparam>
+	/// <param name="input">The input number to convert.</param>
+	/// <returns>The converted <see cref="SignificantNumber"/>.</returns>
 	public static SignificantNumber ToSignificantNumber<TInput>(this INumber<TInput> input)
 		where TInput : INumber<TInput>
 	{
@@ -26,6 +35,13 @@ public static class SignificantNumberExtensions
 			: throw new NotSupportedException();
 	}
 
+	/// <summary>
+	/// Tries to create a <see cref="SignificantNumber"/> from the input.
+	/// </summary>
+	/// <typeparam name="TInput">The type of the input number.</typeparam>
+	/// <param name="input">The input number to create a <see cref="SignificantNumber"/> from.</param>
+	/// <param name="significantNumber">The created <see cref="SignificantNumber"/> if successful, otherwise null.</param>
+	/// <returns>True if the creation was successful, otherwise false.</returns>
 	internal static bool TryCreate<TInput>([NotNullWhen(true)] TInput input, [MaybeNullWhen(false)] out SignificantNumber significantNumber)
 		where TInput : INumber<TInput>
 	{
