a5c775f5e781bf455d81a7bb79cd5ece5ecd177a
Matthew Edmondson <matthew.edmondson@gmail.com>
2025-01-03 12:31:57 +1100

    Inherit from precise number

--- SignificantNumber/SignificantNumberExtensions.cs
+++ SignificantNumber/SignificantNumberExtensions.cs
@@ -1,7 +1,8 @@
 namespace ktsu.SignificantNumber;
 
-using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
+
+using ktsu.PreciseNumber;
 
 /// <summary>
 /// Provides extension methods for converting numbers to <see cref="SignificantNumber"/>.
@@ -17,48 +18,10 @@ public static class SignificantNumberExtensions
 	public static SignificantNumber ToSignificantNumber<TInput>(this INumber<TInput> input)
 		where TInput : INumber<TInput>
 	{
-		// if TInput is already a SignificantNumber then just return it
-		SignificantNumber significantNumber;
 		bool success = typeof(TInput) == typeof(SignificantNumber);
 
-		if (success)
-		{
-			significantNumber = (SignificantNumber)(object)input;
-		}
-		else
-		{
-			success = TryCreate((TInput)input, out significantNumber);
-		}
-
 		return success
-			? significantNumber
-			: throw new NotSupportedException();
-	}
-
-	/// <summary>
-	/// Tries to create a <see cref="SignificantNumber"/> from the input.
-	/// </summary>
-	/// <typeparam name="TInput">The type of the input number.</typeparam>
-	/// <param name="input">The input number to create a <see cref="SignificantNumber"/> from.</param>
-	/// <param name="significantNumber">The created <see cref="SignificantNumber"/> if successful, otherwise null.</param>
-	/// <returns>True if the creation was successful, otherwise false.</returns>
-	internal static bool TryCreate<TInput>([NotNullWhen(true)] TInput input, [MaybeNullWhen(false)] out SignificantNumber significantNumber)
-		where TInput : INumber<TInput>
-	{
-		var type = typeof(TInput);
-		if (Array.Exists(type.GetInterfaces(), i => i.Name.StartsWith("IBinaryInteger", StringComparison.Ordinal)))
-		{
-			significantNumber = SignificantNumber.CreateFromInteger(input);
-			return true;
-		}
-
-		if (Array.Exists(type.GetInterfaces(), i => i.Name.StartsWith("IFloatingPoint", StringComparison.Ordinal)))
-		{
-			significantNumber = SignificantNumber.CreateFromFloatingPoint(input);
-			return true;
-		}
-
-		significantNumber = default;
-		return false;
+			? (SignificantNumber)(object)input
+			: (SignificantNumber)input.ToPreciseNumber();
 	}
 }
